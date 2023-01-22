670e76085448a74e592b2d9cef26c96cf5d7d4b2
ChinaiArman <chinaiarman@gmail.com>
2023-01-21 17:28:41 -0800

    fix openai backend

--- api/controllers/openai.controller.js
+++ api/controllers/openai.controller.js
@@ -1,68 +1,37 @@
 const { Configuration, OpenAIApi } = require("openai");
 
-// Import the Configuration and OpenAIApi classes from the openai package
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
-// Create an instance of the Configuration class with the API key from the environment variable.
-// Create an instance of the OpenAIApi class with the configuration instance.
 
-export default async function (req, res) {
+async function notemaker(article) {
     if (!configuration.apiKey) {
-        res.status(500).json({
-            error: {
-                message:
-                    "OpenAI API key not configured, please follow instructions in README.md",
-            },
-        });
-        return;
+        return "OpenAI API key not configured, please follow instructions in README.md";
     }
 
-    // Check if the configuration instance has an apiKey property. If not, return an error message.
-
-    const article = req.body.article || "";
     if (article.trim().length === 0) {
-        res.status(400).json({
-            error: {
-                message: "Please enter a valid animal",
-            },
-        });
-        return;
+        return "Please enter a prompt";
     }
-
-    // Get the animal parameter from the request body and check if it is a valid input. If not, return an error message.
 
     try {
-        // const completion = await openai.createCompletion({
-        //   model: "text-davinci-003",
-        //   prompt: generatePrompt(animal),
-        //   temperature: 0.6, // 0.6 is the default value
-        // });
         const completion = await openai.createCompletion({
             model: "text-curie-001",
             prompt: generatePrompt(article),
-            temperature: 1, // 0.7 is the default value
+            temperature: 1,
             max_tokens: 60,
             top_p: 1.0,
             frequency_penalty: 0.0,
             presence_penalty: 1,
         });
-        res.status(200).json({ result: completion.data.choices[0].text });
+        return completion.data.choices[0].text;
     } catch (error) {
-        // Consider adjusting the error handling logic for your use case
         if (error.response) {
             console.error(error.response.status, error.response.data);
-            res.status(error.response.status).json(error.response.data);
         } else {
             console.error(`Error with OpenAI API request: ${error.message}`);
-            res.status(500).json({
-                error: {
-                    message: "An error occurred during your request.",
-                },
-            });
         }
     }
 }
@@ -73,15 +42,5 @@ function generatePrompt(article) {
   Summarize with bullet points, keep statistical details:`;
 }
 
-// function generatePrompt(animal) {
-//   const capitalizedAnimal =
-//     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-//   return `Suggest three names for an animal that is a superhero.
 
-// Animal: Cat
-// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-// Animal: Dog
-// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-// Animal: ${capitalizedAnimal}
-// Names:`;
-// }
+module.exports = { notemaker }
--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const port = 5000;
 
 
 // import controllers
-const openAiController = require('./controllers/openai.controller');
+const { notemaker } = require('./controllers/openai.controller');
 
 // Set middleware
 app.use(cors())
@@ -14,13 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
-    console.log('banana')
-    const message = openAiController.complete("Authorization is the process of granting an integration access to a user’s Notion data. That process varies depending on whether or not the integration is public or internal. For specifics on the differences between public and internal integrations, refer to the getting started overview. Read on to learn how to set up the auth flows for internal and public integrations.");
-    console.log('apple')
-    console.log(message)
-    console.log('orange')
-    res.json({ message: message });
+app.get('/', async (req, res) => {
+    // const prompt = req.body.prompt;
+    const prompt = "The quick brown fox jumps over the lazy dog.";
+    const message = await notemaker(prompt);
+    res.json({ notes: message });
 })
 
 
