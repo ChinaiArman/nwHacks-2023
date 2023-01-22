513c4f31898bc4c1f2586f2f938dfa9254dbfc5e
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2023-01-21 23:20:23 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,106 +1,108 @@
 function getSelectedText() {
-    var text = "";
+  var text = "";
 
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        // if they are using internet explorer
-        text = document.selection.createRange().text;
-    }
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    // if they are using internet explorer
+    text = document.selection.createRange().text;
+  }
 
-    return text;
+  return text;
 }
 
 function myFunction() {
-    console.log("in")
-    // Get the text field
-    var copyText = document.getElementById("myInput");
+  console.log("in");
+  // Get the text field
+  var copyText = document.getElementById("myInput");
 
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
 
-    // Copy the text inside the text field
-    navigator.clipboard.writeText('banana');
+  // Copy the text inside the text field
+  navigator.clipboard.writeText("banana");
 
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
 }
 
 async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
 async function onClickHandler() {
-    // clear the results
-    document.getElementById("result-container").innerHTML = "";
+  // show the popup
+  document.querySelector(".bg-card").classList.toggle("hidden");
+  // clear the results
+  document.getElementById("result-container").innerHTML = "";
 
-    // show the loading spinner
-    const loadingSpinner = document.getElementById("loading-spinner");
-    loadingSpinner.classList.remove("hidden");
+  // show the loading spinner
+  const loadingSpinner = document.getElementById("loading-spinner");
+  loadingSpinner.classList.remove("hidden");
 
-    const tab = await getCurrentTab();
+  const tab = await getCurrentTab();
 
-    // executes the script in the context of the current tab
-    const scriptRes = await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: getSelectedText,
-    });
-    const selectedText = scriptRes[0].result;
+  // executes the script in the context of the current tab
+  const scriptRes = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: getSelectedText,
+  });
+  const selectedText = scriptRes[0].result;
 
-    // sends a message to the background script
-    chrome.runtime.sendMessage({ selectedText: selectedText });
+  // sends a message to the background script
+  chrome.runtime.sendMessage({ selectedText: selectedText });
 }
 
 async function onMessageReceived(request, sender, sendResponse) {
-    if (request.notes) {
-        const resultDiv = document.getElementById("result-container");
-        const notes = request.notes;
+  if (request.notes) {
+    const resultDiv = document.getElementById("result-container");
+    const notes = request.notes;
 
-        let formattedResults = "<ul>";
+    let formattedResults = "<ul>";
 
-        notes
-            .split("- ")
-            .slice(1)
-            .forEach((element) => {
-                formattedResults += `<li class="note-bullet">${element}</li>`;
-            });
+    notes
+      .split("- ")
+      .slice(1)
+      .forEach((element) => {
+        formattedResults += `<li class="note-bullet">${element}</li>`;
+      });
 
-        formattedResults += "</ul>";
+    formattedResults += "</ul>";
 
-        resultDiv.innerHTML = formattedResults;
+    resultDiv.innerHTML = formattedResults;
 
-        // hide the loading spinner
-        const loadingSpinner = document.getElementById("loading-spinner");
-        loadingSpinner.classList.add("hidden");
-    }
+    // hide the loading spinner
+    const loadingSpinner = document.getElementById("loading-spinner");
+    loadingSpinner.classList.add("hidden");
+  }
 }
 
-document.getElementById("generate-btn").addEventListener("click", function () {
-  document.querySelector(".bg-card").classList.toggle("hidden");
 async function CopyToClipboard() {
-    var element = document.getElementById("result-container")
-    console.log(element.textContent)
+  var element = document.getElementById("result-container");
+  console.log(element.textContent);
 
-    navigator.clipboard.writeText(element.textContent);
+  navigator.clipboard.writeText(element.textContent);
 }
 
 // Listeners
-document.getElementById("generate-btn").addEventListener("click", onClickHandler);
+document
+  .getElementById("generate-btn")
+  .addEventListener("click", onClickHandler);
 document.getElementById("copy-btn").addEventListener("click", CopyToClipboard);
 document.getElementById("result-container").style.display = "none";
 
 document.getElementById("generate-btn").addEventListener("click", function () {
-    document.getElementById("result-container").style.display = "block";
+  document.getElementById("result-container").style.display = "block";
 });
 
 // Listeners
 document
-    .getElementById("generate-btn")
-    .addEventListener("click", onClickHandler);
+  .getElementById("generate-btn")
+  .addEventListener("click", onClickHandler);
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    onMessageReceived(request, sender, sendResponse);
+  onMessageReceived(request, sender, sendResponse);
 });
